e88364d980f84a9de681cc59ee76ba9ef9abd41e
oh019 <44891378+oh019@users.noreply.github.com>
2019-01-20 11:53:57 +0100

    Add files via upload

--- main.php
+++ main.php
@@ -15,7 +15,7 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <link href="bootstrap-4.1.3-dist/css" rel="stylesheet" id="bootstrap-css">
     <script src="bootstrap-4.1.3-dist/js"></script>
-    <link rel="stylesheet" href="profile.css">
+    <link rel="stylesheet" href="main.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <script> function toggleSidebar() {
             document.getElementById ("profile-usermenu").classList.toggle('active');}</script>
@@ -34,9 +34,9 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
             <li class="nav-item">
                 <a class="nav-link" href="#">Einstellungen</a>
             </li>
-         <!--   <li class="nav-item">
-                <a class="nav-link" href="#"></a>
-            </li>-->
+            <!--   <li class="nav-item">
+                   <a class="nav-link" href="#"></a>
+               </li>-->
             <li class="nav-item">
                 <a class="nav-link" href="#">Abmelden</a>
             </li>
@@ -48,49 +48,52 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
 
 <div class="container">
     <!--<img id="background" src="bilder/hintergrund.jpg" alt="">-->
-        <div class="row">
-          <div class="col-sm-3">
+    <div class="row">
+        <div class="col-sm-3">
             <div class="abstand">
                 <div id="benutzer_name">
                     <h2>
-                    <?php if(isset($_SESSION['angemeldet']))
-                    {
-                        echo $nutzer_ausgelesen;
-                    }?>
+                        <?php if(isset($_SESSION['angemeldet']))
+                        {
+                            echo $nutzer_ausgelesen;
+                        }?>
                     </h2>
 
                 </div>
+                <!-- SIDEBAR Userimage -->
                 <!--Placeholder falls ein Nutzer kein Profilbild hochgeladen hat-->
-            <div class="profile-img"><img id="user_img" src="/bilder/default-user-profile.svg" alt="Profilbild">
-            </div>
-            <div class="change_img">
-                <button type="button" class="btn btn-light btn-sm">
-                <a href="image_upload.php" id="flat_button">Bearbeiten</a> <!-- flat button fehlt css-->
-                </button>
-            </div>
+                <div class="profile-img"><img id="user_img" src="bilder/default-user-profile-picture-3.png" alt="Profilbild">
+                </div>
+                <div class="change_img">
+                    <button type="button" class="btn btn-light btn-sm">
+                        <a href="image_upload.php" id="flat_button">Bearbeiten</a> <!-- flat button fehlt css-->
+                    </button>
+                </div>
+                <!-- END SIDEBAR Userimage -->
 
-                 <div class="nav nav-pills flex-column" id="profile-usermenu">
-                <!--HAMBURGER Menu -->
-                    <div class= "toggle-btn" onclick="toggleSidebar()">
+            <div class="nav nav-pills flex-column" id="profile-usermenu">
+                    <!--HAMBURGER Menu -->
+                  <div class= "toggle-btn" onclick="toggleSidebar()">
                         <span> </span>
                         <span> </span>
                         <span> </span>
                     </div>
-                <!--MENU -->
-                <!--<p> <i class="fas fa-cog"></i> <a href="#" target="_blank"> Freunde</a></p>-->
+                    <!--MENU -->  <!-- SIDEBAR BUTTONS -->
+
                     <p> <i class="fas fa-user-friends"></i> <a href="friends_list.php" target="_blank"> Freunde</a></p>
                     <p> <i class="fas fa-globe"></i> <a href="news_main.php" target="_blank"> Neuigkeiten</a></p>
-                    <p> <i class="far fa-calendar-times"></i> <a href="#" target="_blank"> Stundenplan</a></p>
+                   <!-- <p> <i class="far fa-calendar-times"></i> <a href="#" target="_blank"> Stundenplan</a></p>-->
                     <p> <i class="fas fa-calendar-week"></i> <a href="veranstaltungen_main.php" target="_blank"> Veranstaltungen</a></p>
-                    <p> <i class="fas fa-briefcase"></i> <a href="#" target="_blank"> Jobbörse</a></p>
+                  <!--  <p> <i class="fas fa-briefcase"></i> <a href="#" target="_blank"> Jobbörse</a></p>-->
                     <p> <i class="fas fa-exchange-alt"></i> <a href="#" target="_blank"> Anzeigen</a></p>
-                 </div>
-            </div> <!--<hr class="d-sm-none">-->
+                </div>
+                <!-- END SIDEBAR BUTTONS -->
+            </div>
         </div>
         <div class="col-sm-6">
             <div class="abstand">
                 <!-- Darstellung der Beiträge-->
-                 <div class="space"><h6>Beitrag erstellen</h6></div>
+                <div class="space"><h6>Beitrag erstellen</h6></div>
                 <!--Eingabefeld-->
                 <div class="form-group">
                     <div id="new_post"><form>
@@ -123,9 +126,10 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
 
                     <!--alle Beiträge anzeigen lassen durch einbinden des Codes in 'alle_Beiträge'-->
                     <div id="alle_beitraege">
-                    <?php
-                    include 'alle_beitraege.php'
-                    ?>
+                        <iframe src="alle_beitraege.php" height="500" scrolling="yes" width="500" class="beitraege_iframe"></iframe>
+                        <!--
+                        include 'alle_beitraege.php'
+                        -->
                     </div>
 
 
@@ -142,24 +146,24 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
                 </div>
             </div>
         </div>
-            <div class="col-sm-3">
-                <div class="abstand">
-                    <div class="news">
+        <div class="col-sm-3">
+            <div class="abstand">
+                <div class="news">
                     <h2>Neuigkeiten</h2>
-                         Bla
+                    Bla
 
-                         Bla
+                    Bla
 
-                         Bla
+                    Bla
 
 
-                         Bla
+                    Bla
 
-                    </div>  <!--    <hr class="d-sm-none">-->
-                </div>
+                </div>  <!--    <hr class="d-sm-none">-->
             </div>
         </div>
     </div>
+</div>
 
 
 
@@ -169,3 +173,12 @@ $nutzer_ausgelesen = $_SESSION['aktiveruser_name'];
 
 </body>
 </html>
+
+
+
+
+
+
+
+
+
